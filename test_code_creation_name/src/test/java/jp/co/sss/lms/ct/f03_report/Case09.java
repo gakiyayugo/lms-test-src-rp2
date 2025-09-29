78880d23e01ccfb57a38532f15784f95133234f4
y-gakiya <yugo.gakiya@ftsnet.co.jp>
2025-09-29 14:14:32 +0900

    add:Case09.java ログイン画面へ遷移、コース詳細画面へ遷移、ユーザー詳細画面へ遷移、レポート登録画面へ遷移、不適切な内容で修正を行い、「提出する」ボタンを押下するテストコード追加
    ログイン画面へ遷移するURLを入力し正しいログインID、パスワードを入力した後ログインボタンを押下し、上部メニューから「ようこそ受講生AA２さん」リンクを押下、「2022年10月2日(日)」の「週報【デモ】」の修正ボタンを押下、不適切な内容で修正を行い、「提出する」ボタンを押下した際、期待値通りかどうか確認するテストを作成
    期待値は以下の通り
    入力するURL:http://localhost:8080/lms
    遷移する画面：ログイン画面
    入力するログインID：StudentAA02
    入力するパスワード：Student02
    遷移する画面：コース詳細画面
    クリック：ようこそ受講生AA２さん
    遷移する画面：ユーザー詳細画面
    クリック：「2022年10月2日(日)」の「週報【デモ】」の修正ボタン
    遷移する画面：レポート登録画面
    修正：学習項目を未入力
    選択：理解度を「３」
    クリック：提出する
    エラー表示：学習項目
    文字入力：学習項目に「java」と入力
    選択：理解度を未入力
    クリック：提出する
    エラー表示：理解度
    選択：理解度を「３」
    文字入力：目標の達成度に「A」と入力
    クリック：提出する
    エラー表示：目標の達成度
    文字入力：目標の達成度に「11」と入力
    クリック：提出する
    エラー表示：目標の達成度
    修正：目標の達成度・所感を未入力
    クリック：提出する
    エラー表示：目標の達成度・所感
    文字入力：目標の達成度に「5」と入力
    文字入力：所感・一週間の振り返りに「A」と2001文字入力
    クリック：提出する
    エラー表示：所感・一週間の振り返り
    
    add:Case15.java
    ログイン画面へ遷移、利用規約画面へ遷移、チェックボックスにチェックを入れずに次へボタンを押下するテストコード追加
    
    ログイン画面へ遷移するURLを入力し、初回ログインユーザーの、ログインID、パスワードを入力した後ログインボタンを押下し、「同意します」チェックボックスにチェックせず次へボタンを押下した際、期待値通りかどうか確認するテストを作成
    期待値は以下の通り
    入力するURL:http://localhost:8080/lms
    遷移する画面：ログイン画面
    入力するログインID：StudentAA03
    入力するパスワード：StudentAA03
    遷移する画面：利用規約画面
    クリック：次へ
    エラー表示：セキュリティ規約への同意は必須です。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case09.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case09.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -35,70 +41,276 @@ public class Case09 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		goTo("http://localhost:8080/lms");
+
+		assertEquals("ログイン | LMS", getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		final WebElement id = getElementByName("loginId");
+		id.clear();
+		id.sendKeys("StudentAA02");
+
+		final WebElement pass = getElementByName("password");
+		pass.clear();
+		pass.sendKeys("Student02");
+		pass.sendKeys(Keys.ENTER);
+
+		getEvidence(new Object() {
+		});
+
+		assertEquals("コース詳細 | LMS", getTitle());
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ようこそ○○さん」リンクからユーザー詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		final WebElement user = getElementText("ようこそ受講生ＡＡ２さん");
+		user.click();
+
+		getEvidence(new Object() {
+		});
+
+		assertEquals("ユーザー詳細", getTitle());
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 該当レポートの「修正する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		scrollBy("250");
+
+		final List<WebElement> fix = getElementsBycssSelector("input[value='修正する']");
+		fix.get(0).click();
+
+		pageLoadTimeout(5);
+		getEvidence(new Object() {
+		});
+
+		assertEquals("レポート登録 | LMS", getTitle());
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を修正して「提出する」ボタンを押下しエラー表示：学習項目が未入力")
 	void test05() {
-		// TODO ここに追加
+
+		final List<WebElement> text = getElementsByListClassName("form-control");
+		text.get(0).clear();
+
+		scrollBy("400");
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement submit = getElementBycssSelector("button[type='submit']");
+		submit.click();
+
+		final WebElement error = getElementByclassName("errorInput");
+		String errorcheck = "";
+
+		if (error.isDisplayed()) {
+			errorcheck = "true";
+		}
+
+		assertEquals("true", errorcheck);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：理解度が未入力")
 	void test06() {
-		// TODO ここに追加
+
+		final List<WebElement> text = getElementsByListClassName("form-control");
+		text.get(0).clear();
+		text.get(0).sendKeys("java");
+
+		text.get(1).click();
+		final List<WebElement> understand = getElementsByTagName("option");
+		understand.get(0).click();
+
+		scrollBy("400");
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement submit = getElementBycssSelector("button[type='submit']");
+		submit.click();
+
+		final WebElement error = getElementByclassName("errorInput");
+		String errorcheck = "";
+
+		if (error.isDisplayed()) {
+			errorcheck = "true";
+		}
+
+		assertEquals("true", errorcheck);
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：目標の達成度が数値以外")
-	void test07() {
-		// TODO ここに追加
+	void test07() throws InterruptedException {
+
+		final List<WebElement> text = getElementsByListClassName("form-control");
+		text.get(1).click();
+
+		visibilityTimeout(By.tagName("option"), 5);
+		final List<WebElement> understand = getElementsByTagName("option");
+		understand.get(3).click();
+
+		Thread.sleep(5);
+		text.get(2).clear();
+		text.get(2).sendKeys("A");
+
+		scrollBy("400");
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement submit = getElementBycssSelector("button[type='submit']");
+		submit.click();
+
+		visibilityTimeout(By.className("errorInput"), 5);
+		final WebElement error = getElementByclassName("errorInput");
+		String errorcheck = "";
+
+		if (error.isDisplayed()) {
+			errorcheck = "true";
+		}
+
+		assertEquals("true", errorcheck);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(8)
 	@DisplayName("テスト08 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：目標の達成度が範囲外")
 	void test08() {
-		// TODO ここに追加
+
+		final List<WebElement> text = getElementsByListClassName("form-control");
+		text.get(2).clear();
+		text.get(2).sendKeys("11");
+
+		scrollBy("400");
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement submit = getElementBycssSelector("button[type='submit']");
+		submit.click();
+
+		visibilityTimeout(By.className("errorInput"), 5);
+		final WebElement error = getElementByclassName("errorInput");
+		String errorcheck = "";
+
+		if (error.isDisplayed()) {
+			errorcheck = "true";
+		}
+
+		assertEquals("true", errorcheck);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(9)
 	@DisplayName("テスト09 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：目標の達成度・所感が未入力")
-	void test09() {
-		// TODO ここに追加
+	void test09() throws InterruptedException {
+
+		final List<WebElement> text = getElementsByListClassName("form-control");
+		text.get(2).clear();
+		text.get(3).clear();
+
+		scrollBy("400");
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement submit = getElementBycssSelector("button[type='submit']");
+		submit.click();
+
+		Thread.sleep(5);
+		scrollBy("300");
+
+		visibilityTimeout(By.className("errorInput"), 5);
+		final List<WebElement> error = getElementsByListClassName("errorInput");
+
+		String errorcheck = "";
+
+		if (error.get(0).isDisplayed()) {
+			errorcheck = "true";
+		}
+
+		assertEquals("true", errorcheck);
+
+		if (error.get(1).isDisplayed()) {
+			errorcheck = "true";
+		}
+
+		assertEquals("true", errorcheck);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(10)
 	@DisplayName("テスト10 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：所感・一週間の振り返りが2000文字超")
-	void test10() {
-		// TODO ここに追加
+	void test10() throws InterruptedException {
+
+		final List<WebElement> text = getElementsByListClassName("form-control");
+		text.get(2).clear();
+		text.get(2).sendKeys("5");
+
+		text.get(3).clear();
+		text.get(3).sendKeys("A".repeat(2001));
+
+		text.get(4).clear();
+		text.get(4).sendKeys("A".repeat(2001));
+
+		Thread.sleep(5);
+		scrollBy("400");
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement submit = getElementBycssSelector("button[type='submit']");
+		submit.click();
+
+		Thread.sleep(5);
+		scrollBy("400");
+
+		visibilityTimeout(By.className("errorInput"), 5);
+		final List<WebElement> error = getElementsByListClassName("errorInput");
+
+		String feelerrorcheck = "";
+		String weekerrorcheck = "";
+
+		if (error.get(0).isDisplayed()) {
+			feelerrorcheck = "true";
+		}
+
+		assertEquals("true", feelerrorcheck);
+
+		if (error.get(1).isDisplayed()) {
+			weekerrorcheck = "true";
+		}
+
+		assertEquals("true", weekerrorcheck);
+
+		getEvidence(new Object() {
+		});
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,21 +38,49 @@ public class Case15 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		goTo("http://localhost:8080/lms");
+
+		assertEquals("ログイン | LMS", getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		final WebElement id = getElementByName("loginId");
+		id.clear();
+		id.sendKeys("StudentAA03");
+
+		final WebElement pass = getElementByName("password");
+		pass.clear();
+		pass.sendKeys("StudentAA03");
+		pass.sendKeys(Keys.ENTER);
+
+		getEvidence(new Object() {
+		});
+
+		assertEquals("セキュリティ規約 | LMS", getTitle());
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックをせず「次へ」ボタンを押下")
 	void test03() {
-		// TODO ここに追加
+
+		final WebElement next = getElementBycssSelector("button[type='submit']");
+		next.click();
+
+		final WebElement error = getElementByclassName("error");
+		assertEquals("セキュリティ規約への同意は必須です。", error.getText());
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
