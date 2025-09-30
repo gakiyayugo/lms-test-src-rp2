3f45b786f5150e234cc14fd7d3ebf30b999f19a5
y-gakiya <yugo.gakiya@ftsnet.co.jp>
2025-09-30 09:23:59 +0900

    add:Case16.java ログイン画面へ遷移、利用規約画面へ遷移、パスワード変更画面へ遷移、パスワード入力を行い、「変更」ボタンを押下するテストコード追加
    ログイン画面へ遷移するURLを入力し、初回ログインユーザーの、ログインID、パスワードを入力した後ログインボタンを押下し、「同意します」チェックボックスにチェックを入れ次へボタンを押下したし、不適切なパスワード変更を行い、「変更」ボタンを押下した際、期待値通りかどうか確認するテストを作成
    期待値は以下の通り
    入力するURL:http://localhost:8080/lms
    遷移する画面：ログイン画面
    入力するログインID：StudentAA04
    入力するパスワード：StudentAA04
    遷移する画面：利用規約画面
    チェックボックス：「同意します」にチェック
    クリック：次へ
    遷移する画面：パスワード変更画面
    クリック：変更
    ポップアップでクリック：変更
    エラー表示：現在のパスワードで現在のパスワードは必須です。
    新しいパスワードでパスワードは必須です。
    「パスワード」には半角英数字のみ使用可能です。また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。
    確認パスワードで確認パスワードは必須です。
    文字入力：現在のパスワードに「StudentAA04」
    新しいパスワードに「1234567890Abcdefghijk」
    確認パスワードに「1234567890Abcdefghijk」
    クリック：変更
    ポップアップでクリック：変更
    エラー表示：新しいパスワードでパスワードの長さが最大値(20)を超えています。
    文字入力：現在のパスワードに「StudentAA04」
    新しいパスワードに「1234567890」
    確認パスワードに「1234567890」
    クリック：変更
    ポップアップでクリック：変更
    エラー表示：新しいパスワードで「パスワード」には半角英数字のみ使用可能です。また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。
    文字入力：現在のパスワードに「StudentAA04」
    新しいパスワードに「Abc1234567890」
    確認パスワードに「Aaa1234567890」
    クリック：変更
    ポップアップでクリック：変更
    エラー表示：新しいパスワード：パスワードと確認パスワードが一致しません。
    
    add:Case17.java
    ログイン画面へ遷移、利用規約画面へ遷移、パスワード変更画面へ遷移、パスワード入力を行い、「変更」ボタンを押下するテストコード追加
    
    ログイン画面へ遷移するURLを入力し、初回ログインユーザーの、ログインID、パスワードを入力した後ログインボタンを押下し、「同意します」チェックボックスにチェックを入れ次へボタンを押下したし、適切なパスワード変更を行い、「変更」ボタンを押下した際、期待値通りかどうか確認するテストを作成
    期待値は以下の通り
    入力するURL:http://localhost:8080/lms
    遷移する画面：ログイン画面
    入力するログインID：StudentAA05
    入力するパスワード：StudentAA05
    遷移する画面：利用規約画面
    チェックボックス：「同意します」にチェック
    クリック：次へ
    遷移する画面：パスワード変更画面
    文字入力：現在のパスワードに「StudentAA05」
    新しいパスワードに「Student05」
    確認パスワードに「Student05」
    遷移する画面：コース詳細画面

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case16.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case16.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,49 +41,185 @@ public class Case16 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		goTo("http://localhost:8080/lms");
+
+		assertEquals("ログイン | LMS", getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws InterruptedException {
+
+		final WebElement id = getElementByName("loginId");
+		id.clear();
+		id.sendKeys("StudentAA04");
+
+		final WebElement pass = getElementByName("password");
+		pass.clear();
+		pass.sendKeys("StudentAA04");
+		pass.sendKeys(Keys.ENTER);
+
+		getEvidence(new Object() {
+		});
+
+		Thread.sleep(5);
+		assertEquals("セキュリティ規約 | LMS", getTitle());
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックを入れ「次へ」ボタン押下")
 	void test03() {
-		// TODO ここに追加
+
+		visibilityTimeout(By.cssSelector("input[type='checkbox']"), 5);
+		final WebElement check = getElementBycssSelector("input[type='checkbox']");
+		check.click();
+
+		final WebElement next = getElementBycssSelector("button[type='submit']");
+		next.click();
+
+		getEvidence(new Object() {
+		});
+
+		assertEquals("パスワード変更 | LMS", getTitle());
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 パスワードを未入力で「変更」ボタン押下")
 	void test04() {
-		// TODO ここに追加
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement fix = getElementBycssSelector("button[type='submit']");
+		fix.click();
+
+		visibilityTimeout(By.id("upd-btn"), 5);
+		final WebElement update = getElementById("upd-btn");
+		update.click();
+
+		getEvidence(new Object() {
+		});
+
+		visibilityTimeout(By.className("error"), 5);
+		final List<WebElement> error = getElementsByListClassName("error");
+
+		assertEquals("現在のパスワードは必須です。", error.get(1).getText());
+
+		String pattern1 = error.get(2).getText();
+		char pattern2 = '「';
+
+		if (pattern1.charAt(0) == pattern2) {
+			assertEquals("「パスワード」には半角英数字のみ使用可能です。また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。\n"
+					+ "パスワードは必須です。", error.get(2).getText());
+		} else {
+			assertEquals("パスワードは必須です。\n「パスワード」には半角英数字のみ使用可能です。"
+					+ "また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。", error.get(2).getText());
+		}
+
+		assertEquals("確認パスワードは必須です。", error.get(3).getText());
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 20文字以上の変更パスワードを入力し「変更」ボタン押下")
-	void test05() {
-		// TODO ここに追加
+	void test05() throws InterruptedException {
+
+		visibilityTimeout(By.className("form-control"), 5);
+		final List<WebElement> form = getElementsByListClassName("form-control");
+		form.get(0).sendKeys("StudentAA04");
+		form.get(1).sendKeys("1234567890Abcdefghijk");
+		form.get(2).sendKeys("1234567890Abcdefghijk");
+
+		scrollBy("100");
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement fix = getElementBycssSelector("button[type='submit']");
+		fix.click();
+
+		visibilityTimeout(By.id("upd-btn"), 5);
+		final WebElement update = getElementById("upd-btn");
+		update.click();
+
+		visibilityTimeout(By.className("error"), 5);
+		final List<WebElement> error = getElementsByListClassName("error");
+
+		Thread.sleep(5);
+		assertEquals("パスワードの長さが最大値(20)を超えています。", error.get(1).getText());
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 ポリシーに合わない変更パスワードを入力し「変更」ボタン押下")
-	void test06() {
-		// TODO ここに追加 
+	void test06() throws InterruptedException {
+
+		visibilityTimeout(By.className("form-control"), 5);
+		final List<WebElement> form = getElementsByListClassName("form-control");
+		form.get(0).sendKeys("StudentAA04");
+		form.get(1).sendKeys("1234567890");
+		form.get(2).sendKeys("1234567890");
+
+		scrollBy("100");
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement fix = getElementBycssSelector("button[type='submit']");
+		fix.click();
+
+		visibilityTimeout(By.id("upd-btn"), 5);
+		final WebElement update = getElementById("upd-btn");
+		update.click();
+
+		visibilityTimeout(By.className("error"), 5);
+		final List<WebElement> error = getElementsByListClassName("error");
+
+		Thread.sleep(5);
+		assertEquals("「パスワード」には半角英数字のみ使用可能です。また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。", error.get(1).getText());
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 一致しない確認パスワードを入力し「変更」ボタン押下")
-	void test07() {
-		// TODO ここに追加
+	void test07() throws InterruptedException {
+
+		visibilityTimeout(By.className("form-control"), 5);
+		final List<WebElement> form = getElementsByListClassName("form-control");
+		form.get(0).sendKeys("StudentAA04");
+		form.get(1).sendKeys("Abc1234567890");
+		form.get(2).sendKeys("Aaa1234567890");
+
+		scrollBy("100");
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement fix = getElementBycssSelector("button[type='submit']");
+		fix.click();
+
+		visibilityTimeout(By.id("upd-btn"), 5);
+		final WebElement update = getElementById("upd-btn");
+		update.click();
+
+		visibilityTimeout(By.className("error"), 5);
+		final List<WebElement> error = getElementsByListClassName("error");
+
+		Thread.sleep(5);
+		assertEquals("パスワードと確認パスワードが一致しません。", error.get(1).getText());
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,28 +41,80 @@ public class Case17 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		goTo("http://localhost:8080/lms");
+
+		assertEquals("ログイン | LMS", getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws InterruptedException {
+
+		final WebElement id = getElementByName("loginId");
+		id.clear();
+		id.sendKeys("StudentAA05");
+
+		final WebElement pass = getElementByName("password");
+		pass.clear();
+		pass.sendKeys("StudentAA05");
+		pass.sendKeys(Keys.ENTER);
+
+		getEvidence(new Object() {
+		});
+
+		Thread.sleep(5);
+		assertEquals("セキュリティ規約 | LMS", getTitle());
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックを入れ「次へ」ボタン押下")
 	void test03() {
-		// TODO ここに追加
+
+		visibilityTimeout(By.cssSelector("input[type='checkbox']"), 5);
+		final WebElement check = getElementBycssSelector("input[type='checkbox']");
+		check.click();
+
+		final WebElement next = getElementBycssSelector("button[type='submit']");
+		next.click();
+
+		getEvidence(new Object() {
+		});
+
+		assertEquals("パスワード変更 | LMS", getTitle());
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 変更パスワードを入力し「変更」ボタン押下")
 	void test04() {
-		// TODO ここに追加
+
+		visibilityTimeout(By.className("form-control"), 5);
+		final List<WebElement> form = getElementsByListClassName("form-control");
+		form.get(0).sendKeys("StudentAA05");
+		form.get(1).sendKeys("Student05");
+		form.get(2).sendKeys("Student05");
+
+		scrollBy("100");
+
+		visibilityTimeout(By.cssSelector("button[type='submit']"), 5);
+		final WebElement fix = getElementBycssSelector("button[type='submit']");
+		fix.click();
+
+		visibilityTimeout(By.id("upd-btn"), 5);
+		final WebElement update = getElementById("upd-btn");
+		update.click();
+
+		getEvidence(new Object() {
+		});
+
+		assertEquals("コース詳細 | LMS", getTitle());
+
 	}
 
 }
